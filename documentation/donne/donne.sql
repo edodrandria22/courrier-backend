1eac2b8d02f6eb60b567c5b733403c8cc22cd23f
edodrandria22 <randriadode22@gmail.com>
2026-08-20 20:50:07 +0300

    mety marquer lu

--- documentation/donne/donne.sql
+++ documentation/donne/donne.sql
@@ -7,6 +7,11 @@ INSERT INTO roles (id, name,created_at) VALUES (3, 'Externe',NOW());
 INSERT INTO utilisateurs
 (id,role_id, created_at, deleted_at, email, mdp, nom, adresse)
 VALUES
+(1,1, NOW(), NULL, 'admin@gmail.com', '$2y$10$Djns8FgsL.xk2GBACEtJh.Hs1civTyvdGQ9s6gqbSgDN81QkOHvTi', 'Admin', 'Admin');
+
+INSERT INTO utilisateurs
+(id,role_id, created_at, deleted_at, email, mdp, nom, adresse)
+VALUES
 (2,3, NOW(), NULL, 'externe@gmail.com', 'externe', 'Externe', 'Externe');
 
 
