3f0c677811a0239bd90339faec916d25d567b9b5
edodrandria22 <randriadode22@gmail.com>
2026-09-05 12:25:03 +0300

    mety cloturation

--- documentation/donne/commandeReinitialisation.sql
+++ documentation/donne/commandeReinitialisation.sql
@@ -1,10 +1,14 @@
 delete from Historiques;
 delete from Messages;
+delete from detail_personnes;
 delete from courriers;
-update courriers set date_message = null;
+delete from numero_courriers;
+delete from utilisateurs;
 
 ALTER SEQUENCE utilisateurs_id_seq RESTART WITH 1;
 ALTER SEQUENCE roles_id_seq RESTART WITH 1;
 ALTER SEQUENCE courriers_id_seq RESTART WITH 1;
 ALTER SEQUENCE messages_id_seq RESTART WITH 1;
+ALTER SEQUENCE numero_courriers_id_seq RESTART WITH 1;
+ALTER SEQUENCE detail_personnes_id_seq RESTART WITH 1;
 ALTER SEQUENCE historiques_id_seq RESTART WITH 1;
